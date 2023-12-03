eda6dd604085965031c0c652220cb7f353ae389c
Pavan Sai <pavansaidevops7@gmail.com>
2023-12-03 18:18:08 +0530

    roboshop

--- cart.sh
+++ cart.sh
@@ -1,18 +1,3 @@
-# copying the cart service file
-cp cart.service /etc/systemd/system/cart.service
-# disabling the default nodejs and enabling the required nodejs and then installing nodejs
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
-# adding the user and created an directory and also downloading the cart content and then installing required dependencies
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-cd /app
-unzip /tmp/cart.zip
-cd /app
-npm install
-
-systemctl daemon-reload
-systemctl enable cart
-systemctl restart cart
+component=cart
+source common.sh
+func_nodejs
