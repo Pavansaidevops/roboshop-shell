0f998642c43a21162e5e6e1aa745bfc21e287a3b
Pavan Sai <pavansaidevops7@gmail.com>
2023-12-03 09:51:20 +0530

    functions in roboshop

--- common.sh
+++ common.sh
@@ -1,4 +1,32 @@
+func_apppreq(){
+
+  # adding the user and created an directory and also downloading the ${component} content and then installing required dependencies
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Adding Application User <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  useradd roboshop &>>${log}
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Removing existing Application Content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  rm -rf /app &>>${log}
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Creating application Directory <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  mkdir /app &>>${log}
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Downloading application Content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Extracting Application Content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  cd /app
+  unzip /tmp/${component}.zip &>>${log}
+  cd /app
+
+}
+
+func_systemd(){
+
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Starting ${component} Service <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
+
+}
+
 func_nodejs(){
+
  log=/tmp/roboshop.log
 # copying the ${component} service file
 echo -e "\e[37m >>>>>>>>>>>>>>>>> Create ${component} Service file <<<<<<<<<<<<<<<<<<<<<<\e[0m"
@@ -11,19 +39,9 @@ echo -e "\e[37m >>>>>>>>>>>>>>>>> Disabling,Enabling and installing nodeJS <<<<<
 dnf module disable nodejs -y &>>${log}
 dnf module enable nodejs:18 -y &>>${log}
 dnf install nodejs -y &>>${log}
-# adding the user and created an directory and also downloading the ${component} content and then installing required dependencies
-echo -e "\e[37m >>>>>>>>>>>>>>>>> Creating Application User <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-useradd roboshop &>>${log}
-echo -e "\e[37m >>>>>>>>>>>>>>>>> Removing existing Application Content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-rm -rf /app &>>${log}
-echo -e "\e[37m >>>>>>>>>>>>>>>>> Creating application Directory <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-mkdir /app &>>${log}
-echo -e "\e[37m >>>>>>>>>>>>>>>>> Downloading application Content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-echo -e "\e[37m >>>>>>>>>>>>>>>>> Extracting Application Content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cd /app
-unzip /tmp/${component}.zip &>>${log}
-cd /app
+
+func_apppreq
+
 echo -e "\e[37m >>>>>>>>>>>>>>>>> Downloading NodeJS Dependencies <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 npm install &>>${log}
 # installing the mongodb shell
@@ -32,7 +50,33 @@ dnf install mongodb-org-shell -y &>>${log}
 echo -e "\e[37m >>>>>>>>>>>>>>>>> Loading ${component} Schema <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 mongo --host mongodb.pavansai.online </app/schema/${component}.js &>>${log}
 echo -e "\e[37m >>>>>>>>>>>>>>>>> Starting ${component} Service <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-systemctl daemon-reload &>>${log}
-systemctl enable ${component} &>>${log}
-systemctl restart ${component} &>>${log}
+
+func_systemd
+
+}
+
+func_java(){
+
+  # copying the ${component} service file
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Create ${component} Service file <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+  # installing maven
+  echo -e"\e[37m >>>>>>>>>>>>>>>>> Installing Maven <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  dnf install maven -y &>>${log}
+
+  func_apppreq
+
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Build ${component} Service <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  mvn clean package &>>${log}
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Renaming ${component} file name <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  mv target/${component}-1.0.jar ${component}.jar &>>${log}
+  # installing the mysql and loading the schema
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Installing Mysql <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  dnf install mysql -y &>>${log}
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Loading ${component} Schema <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  mysql -h mysql.pavansai.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+  echo -e "\e[37m >>>>>>>>>>>>>>>>> Starting ${component} Service <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+
+  func_systemd
+
 }
--- shipping.sh
+++ shipping.sh
@@ -1,20 +1,3 @@
-# copying the shipping service file
-cp shipping.service /etc/systemd/system/shipping.service
-# installing maven
-dnf install maven -y
-# adding the user and created an directory and also downloading the shipping content and then installing required dependencies
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-# installing the mysql and loading the schema
-dnf install mysql -y
-mysql -h mysql.pavansai.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+component=shipping
+source common.sh
+func_java
