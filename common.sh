8ed05c1de410c7819bf2004cb517def41a2d40a5
Pavan Sai <pavansaidevops7@gmail.com>
2023-12-03 19:25:12 +0530

    added exit status in roboshop

--- common.sh
+++ common.sh
@@ -1,8 +1,21 @@
 log=/tmp/roboshop.log
+func_exit_status(){
+
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e [0m"
+  else
+    echo -e "\e[31m FAILURE \e [0m"
+  fi
+
+}
+
 func_apppreq(){
   # copying the ${component} service file
    echo -e "\e[37m >>>>>>>>>>>>>>>>> Create ${component} Service file <<<<<<<<<<<<<<<<<<<<<<\e[0m"
    cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+
+   func_exit_status
+
   # adding the user and created an directory and also downloading the ${component} content and then installing required dependencies
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Adding Application User <<<<<<<<<<<<<<<<<<<\e[0m"
   useradd roboshop &>>${log}
@@ -12,10 +25,14 @@ func_apppreq(){
   mkdir /app &>>${log}
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Downloading application Content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+  func_exit_status
+
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Extracting Application Content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   cd /app
   unzip /tmp/${component}.zip &>>${log}
-  cd /app
+
+  func_exit_status
 
 }
 
@@ -26,6 +43,8 @@ func_systemd(){
   systemctl enable ${component} &>>${log}
   systemctl restart ${component} &>>${log}
 
+  func_exit_status
+
 }
 
 func_schema_setup(){
@@ -34,16 +53,28 @@ func_schema_setup(){
    # installing the mongodb shell
      echo -e "\e[37m >>>>>>>>>>>>>>>>> Installing Mongodb Client <<<<<<<<<<<<<<<<<<<<<<\e[0m"
      dnf install mongodb-org-shell -y &>>${log}
+
+     func_exit_status
+
      echo -e "\e[37m >>>>>>>>>>>>>>>>> Loading ${component} Schema <<<<<<<<<<<<<<<<<<<<<<\e[0m"
      mongo --host mongodb.pavansai.online </app/schema/${component}.js &>>${log}
+
+     func_exit_status
+
   fi
 
   if [ "${schema_type}" == "mysql" ]; then
      # installing the mysql and loading the schema
        echo -e "\e[37m >>>>>>>>>>>>>>>>> Installing Mysql <<<<<<<<<<<<<<<<<<<<<<\e[0m"
        dnf install mysql -y &>>${log}
+
+       func_exit_status
+
        echo -e "\e[37m >>>>>>>>>>>>>>>>> Loading ${component} Schema <<<<<<<<<<<<<<<<<<<<<<\e[0m"
        mysql -h mysql.pavansai.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+
+       func_exit_status
+
    fi
 }
 
@@ -51,16 +82,23 @@ func_nodejs(){
   # copying the mongo repo file
   echo -e "\e[37m >>>>>>>>>>>>>>>>> create ${component} Mongo repo file <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+
+  func_exit_status
+
   # disabling,enabling and installing the nodejs
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Disabling,Enabling and installing nodeJS <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   dnf module disable nodejs -y &>>${log}
   dnf module enable nodejs:18 -y &>>${log}
   dnf install nodejs -y &>>${log}
 
+  func_exit_status
+
   func_apppreq
 
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Downloading NodeJS Dependencies <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   npm install &>>${log}
+
+  func_exit_status
 
   func_schema_setup
 
@@ -73,10 +111,15 @@ func_java(){
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Installing Maven <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   dnf install maven -y &>>${log}
 
+  func_exit_status
+
   func_apppreq
 
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Build ${component} Service <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   mvn clean package &>>${log}
+
+  func_exit_status
+
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Renaming ${component} file name <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   mv target/${component}-1.0.jar ${component}.jar &>>${log}
 
@@ -91,10 +134,14 @@ func_python(){
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Installing Python <<<<<<<<<<<<<<<<<<\e[0m"
   dnf install python36 gcc python3-devel -y &>>{log}
 
+  func_exit_status
+
   func_apppreq
 
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Installing Python Requirements <<<<<<<<<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>{log}
+
+  func_exit_status
   
   func_systemd
 
@@ -105,12 +152,16 @@ func_go(){
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Installing Golang <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   dnf install golang -y &>>${log}
 
+  func_exit_status
+
   func_apppreq
 
   echo -e "\e[37m >>>>>>>>>>>>>>>>> Build ${component} Service <<<<<<<<<<<<<<<<<<<<<<\e[0m"
   go mod init dispatch &>>${log}
   go get &>>${log}
   go build &>>${log}
+
+  func_exit_status
 
   func_systemd
 
