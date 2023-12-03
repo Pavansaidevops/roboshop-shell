c9097e0ab7eedd411e1f838d01bb51048ec64892
Pavan Sai <pavansaidevops7@gmail.com>
2023-12-03 10:28:17 +0530

    functions in roboshop

--- dispatch.sh
+++ dispatch.sh
@@ -1,18 +1,3 @@
-# copying the dispatch service file
-cp dispatch.service /etc/systemd/system/dispatch.service
-# installing golang
-dnf install golang -y
-# adding the user and creating an directory and also downloading the dispatch content and then installing required dependencies
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-cd /app
-unzip /tmp/dispatch.zip
-cd /app
-go mod init dispatch
-go get
-go build
-
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl restart dispatch
+component=dispatch
+source common.sh
+func_go
